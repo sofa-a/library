ac278f6bc636ae962200c92bb79fbf1883730147
sofa <matassa.sophia@gmail.com>
2024-12-06 19:28:33 +0800

    finished the moduling of the code

--- script.js
+++ script.js
@@ -1,64 +1,89 @@
-const form = document.querySelector("form");
-const add = document.querySelector(".add");
-const container = document.querySelector(".container");
-const library = [];
-
-
-function Book(title, author, pages) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = false;
+class Book {
+    constructor(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = false;
+    }
 }
 
-const createElement = (tag, properties = {}, ...classes) => {
-    const element = document.createElement(tag);
-    Object.assign(element, properties);
-    if (classes.length) element.classList.add(...classes);
-    return element;
-}
+class Library {
+    #form;
 
-const addBook = (book) => {
-    library.push(book);
-    const card = createElement("div", {}, "card");
-    const title = createElement("p", {textContent: book.title}, "title");
-    const author = createElement("p", {textContent: "Author: " + book.author}, "author");
-    const pages = createElement("p", {textContent: "No. Pages: " + book.pages}, "pages");    
-    const read = createElement("button", {textContent: "unread"}, "read");
-    const remove = createElement("button", {textContent: "remove"}, "remove");
+    constructor() {
+        this.#form = document.querySelector("form");
+    }
+
+    addBook = (book) => {
+        const card = this.createElement("div", {}, "card");
+        const title = this.createElement("p", {textContent: book.title}, "title");
+        const author = this.createElement("p", {textContent: "Author: " + book.author}, "author");
+        const pages = this.createElement("p", {textContent: "No. Pages: " + book.pages}, "pages");    
+        const read = this.createElement("button", {textContent: "unread"}, "read");
+        const remove = this.createElement("button", {textContent: "remove"}, "remove");
+        
+        [title, author, pages, read, remove].forEach(element => card.appendChild(element));
+        document.querySelector(".container").appendChild(card);
+        
+        ["title", "author", "pages"].forEach(name => (form.elements[name].value = ""));
+        buttons = document.querySelectorAll('button');
+    }
+
+    initSubmit = () => {
+        this.#form.addEventListener("submit", function(event) {
+            event.preventDefault();
+            const book = new Book(
+                this.#form.elements["title"].value, 
+                this.#form.elements["author"].value, 
+                this.#form.elements["pages"].value);
+            this.addBook(book); 
+        }.bind(this));    
+    }
+
+    createElement = (tag, properties = {}, ...classes) => {
+        const element = document.createElement(tag);
+        Object.assign(element, properties);
+        if (classes.length) element.classList.add(...classes);
+        return element;
+    }
+
+    toggleRead = (target) => {
+        if (target.textContent == "read") target.textContent = "unread";
+        else if (target.textContent == "unread") target.textContent = "read";
+    };
+
+    removeBook = (target) => {
+        const card = target.closest(".card");
+        card.remove();
+    };
+
+    initAdd = () => {
+        document.querySelector(".add").addEventListener("click", function() {
+            document.querySelector("form").style.display = "flex";
+        });
+    }
     
-    [title, author, pages, read, remove].forEach(element => card.appendChild(element));
-    container.appendChild(card);
-    ["title", "author", "pages"].forEach(name => (form.elements[name].value = ""));
-    buttons = document.querySelectorAll('button');
+    initExit = () => {
+        exit.addEventListener("click", function() {
+            document.querySelector("form").style.display = "none";
+        });
+    }
+
+    initReadRemove = () => {
+        document.addEventListener("click", function(event) {
+            const target = event.target;
+            if (target.classList.contains("read")) this.toggleRead(target);
+            else if (target.classList.contains("remove")) this.removeBook(target);
+        }.bind(this));
+    }
 }
 
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    const book = new Book(form.elements["title"].value, form.elements["author"].value, form.elements["pages"].value)
-    addBook(book); 
-});
+const main = () => {
+    const library = new Library();
+    library.initSubmit();
+    library.initAdd();
+    library.initExit();
+    library.initReadRemove();
+}
 
-add.addEventListener("click", function() {
-    form.style.display = "flex";
-});
-
-exit.addEventListener("click", function() {
-    form.style.display = "none";
-});
-
-const toggleRead = (target) => {
-    if (target.textContent == "read") target.textContent = "unread";
-    else if (target.textContent == "unread") target.textContent = "read";
-};
-
-const removeCard = (target) => {
-    const card = target.closest(".card");
-    card.remove();
-};
-
-document.addEventListener("click", function(event) {
-    const target = event.target;
-    if (target.classList.contains("read")) toggleRead(target);
-    else if (target.classList.contains("remove")) removeCard(target);
-});
+main();
